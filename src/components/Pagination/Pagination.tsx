93151e7126a852b08fd2330b7a34e62f606dec7c
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-07-11 09:38:15 +0200

    feat: add scroll check

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
 import { PaginationProps } from "../../types/types";
+import styles from './Pagination.module.css'
 
 const Pagination: React.FC<PaginationProps> = ({itemsPerPage, totalItems}) => {
   const totalPages: number = Math.ceil(totalItems / itemsPerPage)
 
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 10) {
+        console.log('scroll over')
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll',handleScroll)
+  },[]);
+
+  
   return (
-    <>
+    <div className={styles.paginationContainer}>
     <p>Total: {totalPages}</p>
-    </>
+    </div>
   )
 }
 
