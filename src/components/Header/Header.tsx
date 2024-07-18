f3b7f0d727384dc7b44466ceb09aee8fff44941a
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-07-18 12:57:36 +0200

    feat: attach a link to profile page to a user picture

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import NavBar from './NavBar/NavBar';
 import styles from './Header.module.css';
 import UserStore from '../../store/userStore';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [userName, setUserName] = useState<string | undefined>('Guest');
@@ -18,15 +19,16 @@ const Header = () => {
     <div className={styles.headerContainer}>
       <p className={styles.headerTitle}>Welcome to Pokémon Center</p>
       <NavBar />
-      
       <p className={styles.greetingMessage}>Hello {userName}</p>
-      <div className={styles.profilePictureContainer}>
-      <img
-        src={imageUrl}
-        alt="user's profile picture"
-        className={styles.profilePicture}
-      />
-      </div>
+      <NavLink to="/profile">
+        <div className={styles.profilePictureContainer}>
+          <img
+            src={imageUrl}
+            alt="user's profile picture"
+            className={styles.profilePicture}
+          />
+        </div>
+      </NavLink>
     </div>
   );
 };
